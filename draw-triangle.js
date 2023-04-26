b3f036858213e64b5fa778179f4d6ca88bce98e4
Furkan Narin <narin.github@gmail.com>
2023-04-26 22:36:28 +0300

    updated draw triangle

--- draw-triangle.js
+++ draw-triangle.js
@@ -2,10 +2,10 @@ const draw = (symbol, numOfLines) => {
     let str = ''
     // k is the number of occurences of the symbol in one row
     for (let i = 0, k = 1; i <= numOfLines, k < numOfLines + 1; i++, k++) {
-        str += `${symbol.repeat(k)}${'\n'}`
+        str += `${' '.repeat(numOfLines - k)}${` ${symbol}`.repeat(k)}${'\n'}`
     }
     console.log(str)
     return str
 }
 
-draw('*', 5);
+draw('*', 10);
